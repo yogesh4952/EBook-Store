b7ae667edec03eabf8e8a55969882ebe5a8b61e7
Yogesh Shah Thakuri <yogeshshah2063@gmail.com>
2026-09-09 16:15:31 +0545

    fix: fix column

--- server/internal/user/models/user_model.go
+++ server/internal/user/models/user_model.go
@@ -18,7 +18,7 @@ type User struct {
 	Lastname    string `json:"last_name"`
 	Email       string `json:"email" gorm:"unique;not null"`
 	Role        Role   `json:"role" gorm:"check:role IN ('admin','seller','customer');not null;default:'customer'"`
-	PhoneNumber string `json:"phone_number" binding:"required,min=10,max=10"`
+	PhoneNumber string `json:"phone_number" gorm:"not null; unique"`
 }
 
 func (r Role) IsValid() bool {
